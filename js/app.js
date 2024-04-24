bf81bf375a64d177d7d1ce8f448fff6c1c60d684
tal0311 <tal.amit0311@gmail.com>
2024-04-24 15:48:31 +0300

    filter posts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 window.openModal = openModal;
+window.onSearchPosts = onSearchPosts
+
+function onInit(){
+ 
+}
 
 function openModal() {
+
+    sendMsgToParent('display_modal','add_post')
+    
+}
+
+function onSearchPosts(ev){
+  console.log('search', ev.target.value);
+  sendMsgToParent('filter_posts', ev.target.value)
+
+  
+}
+
+function sendMsgToParent(type, payload){
   window.parent.postMessage(
-    { type: 'display_modal', payload: 'add_post' }
+    { type, payload }
     , '*');
 }
